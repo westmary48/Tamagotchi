9a13207029406fd2978fc27dda27d94fe566a4c6
Mary West <westmarye1219@gmail.com>
2019-04-20 12:42:26 -0500

    score is working for all quadrents

--- src/javascripts/components/eat.js
+++ src/javascripts/components/eat.js
@@ -1,13 +1,36 @@
 import util from '../helpers/util';
 
-// let full = 100;
+let full = 100;
+
+const fullAddPoints = () => {
+  full += 10;
+  if (full > 100) {
+    full = 100;
+  }
+  util.printToDom('full-score', full);
+};
+
+const subtractFullPoints = () => {
+  full -= 3;
+  if (full < 0) {
+    full = 0;
+  }
+  util.printToDom('full-score', full);
+};
+
+const eatButtonEvents = () => {
+  document.getElementById('add-full-btn').addEventListener('click', fullAddPoints);
+  document.getElementById('subtract-full-btn').addEventListener('click', subtractFullPoints);
+};
 
 const eatDomStringBuilder = () => {
-let domString = '';
-  domString += `<h3>Eat</h3>`;
-  domString += `<button>Healthy</button>`;
-  domString += `<button>Unhealthy</button>`;
-  util.printToDom('eat', domString)
+  let domString = '';
+    domString += `<h3>Eat</h3>`;
+    domString += `<button id = "add-full-btn">Full</button>`;
+    domString += `<button id = "subtract-full-btn">Starving</button>`;
+    domString += `<div id="full-score">Full Level: ${full}</div>`;
+    util.printToDom('eat', domString);
+    eatButtonEvents();
 
-};
+  };
 export default { eatDomStringBuilder }
