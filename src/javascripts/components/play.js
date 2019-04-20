152b35d26fb6af202b8856fdfeb94de1aaf03e49
Mary West <westmarye1219@gmail.com>
2019-04-20 12:50:52 -0500

    fun button works according to instructions

--- src/javascripts/components/play.js
+++ src/javascripts/components/play.js
@@ -1,37 +1,36 @@
 import util from '../helpers/util';
 
-let play = 100;
+let fun = 50;
 
-const playAddPoints = () => {
-  play += 10;
-  if (play > 100) {
-    play = 100;
+const superFunPoints = () => {
+  fun += 50;
+  if (fun > 100) {
+    fun = 100;
   }
-  util.printToDom('play-score', play);
+  util.printToDom('fun-score', fun);
 };
 
-const subtractPlayPoints = () => {
-  play -= 3;
-  if (play < 0) {
-    play = 0;
+const funPoints = () => {
+  fun += 2;
+  if (fun > 100) {
+    fun = 100;
   }
-  util.printToDom('play-score', play);
+  util.printToDom('fun-score', fun);
 };
 
-const playButtonEvents = () => {
-  document.getElementById('add-play-btn').addEventListener('click', playAddPoints);
-  document.getElementById('subtract-play-btn').addEventListener('click', subtractPlayPoints);
+const funButtonEvents = () => {
+  document.getElementById('add-super-fun-btn').addEventListener('click', superFunPoints);
+  document.getElementById('add-fun-btn').addEventListener('click', funPoints);
 };
 
 const funDomStringBuilder = () => {
   let domString = '';
     domString += `<h3>Fun</h3>`;
-    domString += `<button id = "add-play-btn">Playing</button>`;
-    domString += `<button id = "subtract-play-btn">Bored</button>`;
-    domString += `<div id="play-score">Play Level: ${play}</div>`;
+    domString += `<button id = "add-super-fun-btn">Super Fun!</button>`;
+    domString += `<button id = "add-fun-btn">Fun</button>`;
+    domString += `<div id="fun-score">Fun Level: ${fun}</div>`;
     util.printToDom('play', domString);
-    playButtonEvents();
+    funButtonEvents();
 
   };
 export default { funDomStringBuilder }
-
